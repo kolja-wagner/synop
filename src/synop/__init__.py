8f93995cc643c989391897426c775e725730a2f5
Kolja Wagner <k.wagner@stud.uni-hannover.de>
2025-01-25 00:08:15 +0100

    update version 0.2, move version to __init__

--- src/synop/__init__.py
+++ src/synop/__init__.py
@@ -1,7 +1,8 @@
 #
+__version__ = "0.2"
 from . path import PATH_DATA
 
 from . import location
 from . location import Location
 
-from . source import OGIMET, SYNOP
+from . source import OGIMET, SYNOP_PARAM
