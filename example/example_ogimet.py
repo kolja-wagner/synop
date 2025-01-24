981676196bc20f41cbddd85e601af50551b2ed29
Kolja Wagner <k.wagner@stud.uni-hannover.de>
2025-01-24 22:41:53 +0100

    adding example: download locations/synop data

---
+++ example/example_ogimet.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 22:34:11 2025
+
+@author: Kolja
+"""
+from datetime import datetime
+from synop import OGIMET, SYNOP
+        
+def main():
+    
+    # test stations
+    o = OGIMET()
+    stations = o.request_stations(state="Germany")
+    stations.info()
+    
+    # test full year
+    s = SYNOP.block_full_year("10338", 2024)#.url()
+    table = o.request_synop(s, reload=False)
+    table.info()
+    
+    # test full state    
+    s = SYNOP.state_full_day("Germany", datetime(2024,8,1))
+    table = o.request_synop(s, reload=False)
+    table.info()
+    
+    
+if __name__ == "__main__":
+    main()
